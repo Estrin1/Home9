7b76c9014fb3ba1a9ea444249ee3928d75fcd0a6
Evgeniy <estrin.j@yandex.ru>
2022-12-17 12:50:00 +0300

    +2 zadachi

---
+++ z66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 66: Задайте значения M и N. Напишите рекурсивный метод, 
+// который найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void Zadacha66()
+{
+    Console.WriteLine("Введите M ");
+    int M = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите N ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    
+     
+   void RecSumm(int a, int b, int summ=0)
+    {
+        
+        if (a > b) 
+        {Console.WriteLine(summ); 
+        return ;
+        }
+        summ += a;
+        RecSumm(a+1, b,summ);
+        
+    }
+    RecSumm(M, N);
+}
+Zadacha66();
---
+++ z68/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с 
+// помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+//A[m,n]=A[m-1,A[m,n-1]] if  m> 0 N>0
+//n+1 if m=0
+//A(m-1,1) if m>0 & n=0;
+void Zadacha68()
+{
+    Console.WriteLine("Введите M ");
+    int M = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите N ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    int result =  RecAccerman(M, N);
+    Console.WriteLine("\n" + "Значение функции " + result);
+}
+
+    int RecAccerman(int m, int n)
+    {
+        
+        if (m == 0) return n + 1;
+        else if (n == 0) return RecAccerman(m - 1, 1);
+        else   return RecAccerman(m - 1, RecAccerman(m, n - 1));
+    }
+    
+
+Zadacha68();
